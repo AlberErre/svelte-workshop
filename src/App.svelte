5ba6c41af09e8b699a277f50e1a7111afe2b72c3
Alber Erre <arblugo@gmail.com>
2020-11-12 19:06:44 +0000

    feat: init svelte using degit

---
+++ src/App.svelte
@@ -0,0 +1,60 @@
+<script>
+  // Copy and paste this code into https://svelte.dev/repl
+  //
+  // Requirements:
+  // 1. Filling an input should select the next input
+  // 2. Focusing an input should select its content
+  // 3. Pressing backspace should focus and select the contents of the previous input
+  // 4. Pasting the code in the first input should paste each digit in the correct input
+  // 5. Auto submit the form if all fields are filled after a paste
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+  }
+  form {
+    border: 1px solid black;
+    padding: 20px;
+    width: 500px;
+  }
+  .inputs {
+    display: grid;
+    gap: 10px;
+    grid-template-columns: repeat(6, 1fr);
+    margin-bottom: 10px;
+  }
+  .inputs > * {
+    border: 1px solid black;
+    width: 100%;
+    padding: 20px;
+    text-align: center;
+    font-size: 20px;
+    line-height: 1;
+  }
+  button {
+    border: 1px solid black;
+    line-height: 1;
+    background: black;
+    color: white;
+    font-size: 20px;
+    padding: 20px;
+    width: 100%;
+    text-align: center;
+  }
+  button:focus {
+    border-color: blue;
+  }
+</style>
+
+<form>
+  <div class="inputs">
+    <input type="text" name="n1" maxlength="1" />
+    <input type="text" name="n2" maxlength="1" />
+    <input type="text" name="n3" maxlength="1" />
+    <input type="text" name="n4" maxlength="1" />
+    <input type="text" name="n5" maxlength="1" />
+    <input type="text" name="n6" maxlength="1" />
+  </div>
+  <button type="submit">Verify</button>
+</form>
